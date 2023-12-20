cab090de04560ea17dc2841ff566835b9a4d24aa
teiabessa <152933762+teiabessa@users.noreply.github.com>
2023-12-19 22:33:10 -0300

    Update main.tf3

--- main.tf
+++ main.tf
@@ -5,20 +5,22 @@ data "aws_ami" "app_ami" {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
-
-  filter {
+   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
 
   owners = ["979382823631"] # Bitnami
 }
-
+ 
+data "aws_vpc" "default"{
+    default = true
+ }
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instace_type
 
- vpc_security_group_id        = [aws_security_group.blog.id]
+ vpc_security_group_id  = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
@@ -31,11 +33,33 @@ resource "aws_security_group" "blog" {
   vpc_id        = data.aws_vpc.default.id
 }
 
+#http protocol
 resource "aws_security_group_rule" "blog_http_in" {
   type= "ingress"
   from_port          = 80
   to_port            =80
   protocol           = "tcp"
+  cidr_bloks         =["0.0.0.0/0"]
+
+  security_group_id   = aws_security_group_blog.id
+}
+#https protocol
+resource "aws_security_group_rule" "blog_https_in" {
+  type               = "ingress"
+  from_port          = 443
+  to_port             =443
+  protocol           = "tcp"
+  cidr_bloks         =["0.0.0.0/0"]
+
+  security_group_id   = aws_security_group_blog.id
+}
+
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type= "egress"
+  from_port          = 0
+  to_port            = 0
+  protocol           = "-1"
   cidr_bloks         =["0.0.0.0/0"]
 
   security_group_id   = aws_security_group_blog.id
