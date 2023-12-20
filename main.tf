5a2c99c72cb3bd22061677b08f22bfe199ccbb81
teiabessa <152933762+teiabessa@users.noreply.github.com>
2023-12-19 23:41:32 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
- vpc_security_group_id = [aws_security_group.blog.id]
+ vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
@@ -39,9 +39,9 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port          = 80
   to_port            =80
   protocol           = "tcp"
-  cidr_bloks         =["0.0.0.0/0"]
+  cidr_blocks         =["0.0.0.0/0"]
 
-  security_group_ids   = aws_security_group.blog.id
+  security_group_id   = aws_security_group.blog.id
 }
 #https protocol
 resource "aws_security_group_rule" "blog_https_in" {
@@ -49,7 +49,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port          = 443
   to_port             =443
   protocol           = "tcp"
-  cidr_bloks         =["0.0.0.0/0"]
+  cidr_blocks         =["0.0.0.0/0"]
 
   security_group_id   = aws_security_group.blog.id
 }
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port          = 0
   to_port            = 0
   protocol           = "-1"
-  cidr_bloks         =["0.0.0.0/0"]
+  cidr_blocks         =["0.0.0.0/0"]
 
   security_group_id   = aws_security_group.blog.id
 }
