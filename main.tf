2e7169e8866abb4ae8f0ffa1a7a996eea8095b70
teiabessa <152933762+teiabessa@users.noreply.github.com>
2023-12-20 23:29:45 -0300

    create new dev vpc

--- main.tf
+++ main.tf
@@ -40,7 +40,9 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
- tags = {
+  subnet_id = module.blog_vpc.public_subnet[0]
+
+  tags = {
     Name = "Learning Terraform"
   }
 }
@@ -64,7 +66,7 @@ resource "aws_security_group" "blog" {
   name          = "blog"
   description   = "Allow http and https in. Allow everything out"
 
-  vpc_id        = data.aws_vpc.default.id
+  vpc_id        = module.blog_vpc.vpc_id
 
 }
 
